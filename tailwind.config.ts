018b65e1ffde56be9e68da21989a47ddbb367367
Quang Nguyen <ndquang191@gmail.com>
2024-03-04 12:22:46 +0700

    before install shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		colors: {
+			primary: "#ECAD48",
+			p7: "#DCA245",
+			success: "#059669",
+			white: "#FFFFFF",
+			input: "#A0ABBB",
+		},
+		extend: {
+			backgroundImage: {
+				banner: "url('/img/banner/cover.png')",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
